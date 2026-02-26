c566a914a65089283435a5d95b249f6674d56802
Nikhil Raj <122207657+NikForTech@users.noreply.github.com>
2026-02-27 01:12:03 +0530

    server js path change

--- netlify/function/api.js
+++ netlify/function/api.js
@@ -1,4 +1,8 @@
 const serverless = require('serverless-http');
-const app = require('../../server');
+const path = require('path');
+
+// path.resolve finds server.js reliably regardless of where
+// Netlify runs this function from during bundling
+const app = require(path.resolve(__dirname, '..', '..', 'server'));
 
 module.exports.handler = serverless(app);
