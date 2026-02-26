3b07b17503e2816e8d9736e90736979877f74664
Nikhil Raj <122207657+NikForTech@users.noreply.github.com>
2026-02-27 01:05:05 +0530

    change for api
    Simplify the export of the serverless handler.

--- netlify/function/api.js
+++ netlify/function/api.js
@@ -1,14 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('../../server');
 
-// Wrap Express app as a Netlify serverless function
-const handler = serverless(app, {
-  basePath: '/.netlify/functions/api',
-  request: (request, event, context) => {
-    // Strip the function base path so Express sees clean routes
-    // e.g. /.netlify/functions/api/upload → /upload
-    request.url = request.url.replace(/^\/.netlify\/functions\/api/, '') || '/';
-  },
-});
-
-module.exports = { handler };
+module.exports.handler = serverless(app);
